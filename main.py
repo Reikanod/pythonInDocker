b2b0d7db893b43b7263485d25b328731a967e58a
Rey <reikanod@yandex.ru>
2025-06-05 15:50:39 +0300

    dslkfj

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+from fastapi import FastAPI, Request
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    print("✅ main.py загружен")
     return {"message": "Server is running"}
+
+
+@app.post("/get_news")
+async def get_news(request: Request):
+    data = await request.json()
+
+    api_id = data.get("api_id")
+    api_hash = data.get("api_hash")
+    password = data.get("Password")
+
+    # Здесь пока просто выводим в ответ то, что пришло
+    return {
+        "api_id": api_id,
+        "api_hash": api_hash,
+        "password": password
+    }
 
