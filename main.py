09634a70b762406909458fcbe168c801fcf293de
Богдан Христенко <167430641+Reikanod@users.noreply.github.com>
2025-06-05 16:05:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ def read_root():
     return {"message": "Server is running"}
 
 
-@app.post("/get_news")
+@app.post("/get_news/")
 async def get_news(request: Request):
     data = await request.json()
 
     api_id = data.get("api_id")
     api_hash = data.get("api_hash")
     password = data.get("Password")
-    print(data)
-
+    
     # Здесь пока просто выводим в ответ то, что пришло
     return {
         "api_id": api_id,
